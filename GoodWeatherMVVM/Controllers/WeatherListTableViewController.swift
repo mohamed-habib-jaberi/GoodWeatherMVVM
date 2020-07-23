d6dc6ba427dedf0b219d933f30dbf3e50ecd58d1
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-07-23 17:18:13 +0200

    Displaying Hard-Coded Weather into TableView

--- GoodWeatherMVVM/Controllers/WeatherListTableViewController.swift
+++ GoodWeatherMVVM/Controllers/WeatherListTableViewController.swift
@@ -16,4 +16,25 @@ class WeatherListTableViewController: UITableViewController {
            
            self.navigationController?.navigationBar.prefersLargeTitles = true
        }
+    
+    // MARK: - Table view data source
+       override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+           return 100
+       }
+       override func numberOfSections(in tableView: UITableView) -> Int {
+           return 1
+       }
+       
+       override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return 1
+       }
+       
+       
+       override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath) as! WeatherCell
+        
+        cell.cityNameLabel?.text = "Houston"
+        cell.temperatureLabel?.text = "17°"
+           return cell
+       }
 }
---
+++ GoodWeatherMVVM/Custom Cells/WeatherCell.swift
@@ -0,0 +1,29 @@
+//
+//  WeatherCell.swift
+//  GoodWeatherMVVM
+//
+//  Created by mohamed  habib on 23/07/2020.
+//  Copyright © 2020 mohamed  habib. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UITableViewCell {
+
+
+    @IBOutlet weak var cityNameLabel: UILabel!
+    
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

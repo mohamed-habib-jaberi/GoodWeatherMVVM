049c8ed5e8e4d362c31700f34d30b13f0356d087
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-07-24 21:44:20 +0200

    Implementing Checkmark to Indicate the Selected Setting

--- GoodWeatherMVVM/Controllers/SettingsTableViewController.swift
+++ GoodWeatherMVVM/Controllers/SettingsTableViewController.swift
@@ -11,25 +11,30 @@ import UIKit
 class SettingsTableViewController: UITableViewController {
     
     private var settingsViewModel = SettingsViewModel()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         self.navigationController?.navigationBar.prefersLargeTitles = true
     }
-
+    
+    // MARK: - Action
+    @IBAction func done(){
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-       
+        
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       
+        
         return self.settingsViewModel.units.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsCell", for: indexPath)
@@ -40,8 +45,20 @@ class SettingsTableViewController: UITableViewController {
         
         return cell
     }
-
     
-
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+       
+        if let cell = self.tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = .checkmark
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        if let cell = self.tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = .none
+        }
+    }
+    
     
 }

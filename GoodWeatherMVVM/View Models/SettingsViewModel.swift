b20dfb095a3383cc6803ee1536393721c39df06c
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-07-24 21:20:11 +0200

    Implementing Settings View Model

---
+++ GoodWeatherMVVM/View Models/SettingsViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  SettingsViewModel.swift
+//  GoodWeatherMVVM
+//
+//  Created by mohamed  habib on 24/07/2020.
+//  Copyright © 2020 mohamed  habib. All rights reserved.
+//
+
+import Foundation
+
+
+//MARK: Unit Enum
+
+enum Unit: String, CaseIterable {
+    case celsius = "metric"
+    case fahrenheit = "imperial"
+}
+
+// Display in TableView Celsius and Fahrenheit
+extension Unit {
+    
+    var displayName: String {
+        get{
+            switch self {
+            case .celsius:
+                return "Celsius"
+            case .fahrenheit:
+                return "Fahrenheit"
+            
+            }
+        }
+    }
+    
+}
+
+struct SettingsViewModel {
+    
+    let unit = Unit.allCases
+}
